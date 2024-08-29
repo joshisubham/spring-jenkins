e3bb5c632586af8b45c60bad1551ea2d13b4d4c6
Subham <subhamjoshi466@gmail.com>
2024-08-29 15:18:18 +0530

    Added Simple Logging

--- src/main/java/com/project/springboot/controllers/HomeController.java
+++ src/main/java/com/project/springboot/controllers/HomeController.java
@@ -1,12 +1,16 @@
 package com.project.springboot.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+	
+	Logger LOG = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping(method=RequestMethod.GET, value="/home")
 	public String home() {
 		return "At Home";
